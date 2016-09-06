56ee965f54759366041603c95e92e2cb8c5bc60d
DuminduTraa <dumindutissera@gmail.com>
2016-09-06 18:35:06 +0800

    sdflkjsdfkjd

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,11 @@
+
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        increment();
+        displayPrice();
+    }
+
+    private void increment() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        int current_number = Integer.parseInt(quantityTextView.getText().toString());
+        int new_number = current_number+1;
+        quantityTextView.setText("" + new_number);
+    }
+
+    private void displayPrice(){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        int qty = Integer.parseInt(quantityTextView.getText().toString());
+        int price = 5;
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(qty*price));
     }
 }
